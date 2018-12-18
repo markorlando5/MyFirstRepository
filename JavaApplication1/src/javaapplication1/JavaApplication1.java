781832031268cf68062d2f425b8fc12934193808
xcode <xcode@192.168.0.19>
2018-12-18 17:48:11 -0500

    changed to go to bed

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Rise and Shine");
+        System.out.println("Time for bed");
     }
     
 }
